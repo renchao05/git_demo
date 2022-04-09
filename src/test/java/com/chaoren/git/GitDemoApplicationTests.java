7bf69a9d6ae5d91bca8b04f12d2db7fee37cc216
renchao <renchao@admin.com>
2022-04-09 11:04:01 +0800

    master第 3 次提交全部 2.0

--- src/test/java/com/chaoren/git/GitDemoApplicationTests.java
+++ src/test/java/com/chaoren/git/GitDemoApplicationTests.java
@@ -9,6 +9,7 @@ class GitDemoApplicationTests {
     @Test
     void contextLoads() {
         System.out.println("Hello Git! 第 1 版!!");
+        System.out.println("Hello Git! 第 2 版!!");
     }
 
 }
