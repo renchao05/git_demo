c1acb9ab315d56a1aa9c3cb0290526847c686cb7
renchao <renchao@admin.com>
2022-04-09 10:50:17 +0800

    master第 2 次提交

---
+++ src/test/java/com/chaoren/git/GitDemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.chaoren.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("Hello Git! 第 1 版!!");
+    }
+
+}
